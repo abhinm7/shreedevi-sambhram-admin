6bee4ccc370305f7f56e599d127ca0e9fba0a180
ABHIN M <abhin18m@gmail.com>
2024-11-28 15:11:29 +0530

    minor fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div className="navbar">
         <div className="logo">
           <img src="/sambhram_logo.png" alt="" />
-          <h2>Admin Panel</h2>
+          <h2>ADMIN </h2>
         </div>
         <div className="nav-right">
           {token && (
